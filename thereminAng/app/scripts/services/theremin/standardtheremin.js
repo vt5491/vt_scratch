4c47bad92bb28d2064ea039e768326971e74427e
vt5491 <vt5491@gmail.com>
2015-12-30 05:18:01 -0800

    more files

---
+++ thereminAng/app/scripts/services/theremin/standardtheremin.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name thereminAngApp.standardTheremin
+ * @description
+ * # standardTheremin
+ * Service in the thereminAngApp.
+ */
+angular.module('thereminAngApp')
+  //.controller('MainCtrl',['$scope', 'thereminApp', function ($scope, thereminApp) {
+  .service('standardTheremin', [ 'theremin', function ( theremin) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+    //var ChildService = Object.create(BaseService);
+    //var constr = [];
+    //var contr = Object.create()
+    // inherit off base class
+    var constr = Object.create(theremin);
+
+    constr.posSensitivityFactor = 50;
+    
+    // constr.init = function (parms) {
+    //   maxFreq = parms.maxFreq;
+    // };
+    
+    constr.mapFreq = function (pos, maxFreq, posSensitivityFactor) {
+    //constr.prototype.mapFreq = function (pos) {
+      //console.log('theremin.MAX_FREQ=', theremin.getMaxFreq());
+      //console.log('standardTheremin.mapFreq: this.MAX_FREQ=', this.getMaxFreq());
+      console.log('standardTheremin.mapFreq: this.MAX_FREQ=', maxFreq);
+
+      var freq;
+
+      //freq = theremin.getMaxFreq() - Math.abs(pos * this.posSensitivityFactor);
+      //freq = this.getMaxFreq() - Math.abs(pos * this.posSensitivityFactor);
+      //freq = maxFreq - Math.abs(pos * this.posSensitivityFactor);
+      freq = maxFreq - Math.abs(pos * posSensitivityFactor);
+
+      return freq;
+    };
+
+
+    // constr.frameHandler = function (frame) {
+    //   this.ctrlFrameHandler(frame, this.)
+    // };
+    // $scope.$on('freqMapEvent', function (pos) {
+    //   var freq;
+    //   //body...
+    //   freq = this.mapFreq(pos);
+
+    //   $scope.$emit('freqMapEvent-response', freq);
+    // });
+    
+    return constr;
+    
+  }]);
+
---
+++ thereminAng/test/spec/services/theremin/standardtheremin.js
@@ -0,0 +1,56 @@
+'use strict';
+
+describe('Service: standardTheremin', function () {
+
+  // load the service's module
+  beforeEach(module('thereminAngApp'));
+
+  // instantiate service
+  // var standardTheremin;
+  // beforeEach(inject(function (_standardTheremin_) {
+  //   console.log('beforeEach: now in zeroeth beforeEach');
+  //   standardTheremin = _standardTheremin_;
+  // }));
+
+  var mockTheremin, standardTheremin;
+  
+  beforeEach(function(){
+    console.log('beforeEach: now in first beforeEach');
+    module(function($provide){
+      $provide.service('theremin', function(){
+        this.getMaxFreq = function () { return 2500;};
+        //this.abc = function () { return 7;};
+      });
+    });
+    //module('thereminAngApp');
+  });
+
+  // beforeEach(function(){
+  //   console.log('now in beforeEach 2');
+  // });
+  
+  beforeEach(inject(function(_theremin_, _standardTheremin_){
+    console.log('now in beforeEach 3');
+    //console.log('beforeEach: _standardTheremin_=', _standardTheremin_);
+    mockTheremin = _theremin_;
+    standardTheremin = _standardTheremin_;
+    standardTheremin.posSensitivityFactor = 50;
+  }));
+
+  // it('should do something', function () {
+  //   expect(!!standardTheremin).toBe(true);
+  // });
+
+  // it('init works properly', function () {
+  //   standardTheremin.init(parms);
+  
+  //   expect(standardTheremin.mapFreq()).toEqual(7);
+  // });
+
+  it('mapFreq works properly', function () {
+    //console.log('ut: standardTheremin=', standardTheremin);
+    //console.log('ut: standardTheremin.theremin.abc()=', standardTheremin.theremin);
+    expect(standardTheremin.mapFreq(10)).toEqual(2000);
+  });
+
+});
