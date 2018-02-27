231443ac2b438165a5d3a3a59040b453d7ba26c9
vt5491 <vt5491@gmail.com>
2018-02-26 21:35:33 -0800

    test commit.

--- tmp/dummy.js
+++ tmp/dummy.js
@@ -4,3 +4,4 @@
 // line added under windows after repo ssh
 // line added unix, added to /usr/local/share/ca-certificates
 // victory lap?  can I push this from magit?
+// 2018-02-26: test new git credentials manager.
